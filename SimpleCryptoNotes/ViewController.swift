fb11eeac40df94eba098b79a83104f23525fcc57
Pavel Shirokiy <pshirokiy@gmail.com>
2023-05-15 16:11:05 +0300

    Initial Commit

---
+++ SimpleCryptoNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCryptoNotes
+//
+//  Created by Павел Широкий on 15.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
